7979e3b4a1e29705dd9b6990596d93fcd2da23aa
Siyi Zheng <syzheng206265@outlook.com>
2024-10-25 16:32:48 +0100

    Include formula for calculating capture area

--- Powerplant.m
+++ Powerplant.m
@@ -1,3 +1,29 @@
 clear
 clc
 
+% Pitot Inlet
+
+% Manufacturer Engine data
+mdot = 2559; % takeoff mass flow rate in lbs/sec
+D_fan = 111.1; % fan diamter in inches
+fprintf("Fan diamter: %.3fm \n",D_fan*0.0254)
+
+% Capture Area 
+
+% according to raymer pg300 graph
+A_c = mdot*0.025; % ft^2
+D_c = sqrt(A_c/pi)*2*0.3048; % metre
+fprintf("Capture area diameter (empirical): %.3fm \n",D_c)
+
+% isentropic flow
+M_engine = 0.4; % the speed in front of the engine fan
+M_cruise = 0.83;
+M_t = (M_cruise + M_engine)/2; % assumed to be half, according to Raymer and last year's report
+
+A_ratio = (1/M_t*( (1+0.2*M_t^2)/1.2 )^3 ) / (1/M_engine*( (1+0.2*M_engine^2)/1.2 )^3);
+D_ratio = sqrt(A_ratio);
+D_t = D_ratio*D_fan*0.0254; % metre
+fprintf("Capture area diameter (isentropic): %.3fm \n",D_t)
+
+% Nozzle
+

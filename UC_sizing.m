7feec280d2ffd1ba351b87915fb21acd8f952969
Siyi Zheng <syzheng206265@outlook.com>
2024-11-14 23:50:34 +0000

    Nose tire pressure, oleo sizing

--- UC_sizing.m
+++ UC_sizing.m
@@ -7,7 +7,7 @@ L_ws = 65; % wing span in METRE
 
 %Weights
 W_o = 390057;        %MTOW in KG
-W_ng = 0.09*W_o;      %Nose gear weight carrying capability
+W_ng = 0.085*W_o;      %Nose gear weight carrying capability
 W_mg = W_o - W_ng;   %Main gear weight carrying capability
 
 MAC = 7.41; %mean aerodynamic chord in METRE
@@ -25,7 +25,7 @@ x_ng = (W_o*x_cg - W_mg*x_mg)/W_ng;         %x distance of nose gears
 %Main Gear Height
 beta = 15; % tipback angle in degree
 gamma = 15; % rotation clearance angle
-H = atan(deg2rad(gamma))*(L_plane-x_mg-200/39.37); % correction for the rear main gear
+H = tan(deg2rad(gamma))*(L_plane-x_mg-200/39.37); % correction for the rear main gear
 tip_back_angle = rad2deg(tan((x_mg-x_cg)/H)); % between 15 ~ 25 degree
 %check wing tip do not hit the ground at 5 degree roll and AOA 90% Cl_max
 
@@ -65,17 +65,25 @@ Wth_mw =  0.1043*W_mw_lbs^0.48; %Width of main wheel in INCH
 
 %Tire Selection (From a table on Raymer's book), based on Max loading
 % Main Wheel
-%Three-part Name Tire: 37 x 14.0-14
-R_r = 21.3; % Rolling Radius in INCH
-w_tire = 20.5;
-d_tire = 52;
+%Three-part Name Tire: 52 x 20.5-23
+R_r_m = 21.3; % Rolling Radius in INCH
+w_tire_m = 20.5;
+d_tire_m = 52;
 
 % Nose Wheel
+R_r_n = 21.3; % Rolling Radius in INCH
+w_tire_n = 20.5; % width
+d_tire_n = 52; % diameter
 
 
 %Tire Pressure
-A_p = 2.3*sqrt(w_tire*d_tire)*(d_tire/2 - R_r); %wheel contact area in inch^2
-P_mw = W_mw_lbs/A_p; % tire pressure in PSI
+% Main Tire
+A_p_mw = 2.3*sqrt(w_tire_m*d_tire_m)*(d_tire_m/2 - R_r_m); %wheel contact area in inch^2
+P_mw = W_mw_lbs/A_p_mw; % tire pressure in PSI
+
+% Nose Tire
+A_p_nw = 2.3*sqrt(w_tire_n*d_tire_n)*(d_tire_n/2 - R_r_n); %wheel contact area in inch^2
+P_nw = W_nw_lbs/A_p_nw; % tire pressure in PSI
 
 
 %Brake
@@ -87,7 +95,7 @@ KE_per_wheel = KE/16; % KE required for each wheel with break
 %Stroke
 V_vertical = 12; % vertical touchdown speed in feet per second
 N_gear = 2.7; % gear loading factor 2.7 - 3
-S_T = D_mw/2 - R_r; % stroke of tire (half radius - rolling radius)
+S_T = D_mw/2 - R_r_m; % stroke of tire (half radius - rolling radius)
 n_tire = 0.47; % shock efficiency of tire
 n = 0.85; % shock absorber efficiency
 % unit conversion
@@ -100,9 +108,12 @@ S = (V_vertical)^2 / (2*9.81*n*N_gear) - n_tire/n * S_T; % should be around 25-3
 L_oleo = 2.5*S; % length of oleo
 P_oleo = 12415e3; % internal pressure oleo as in Raymers
 % Main wheel
-W_moleo = W_mg*9.81/4; % Load on wheel in Newton
+W_moleo = W_mg_max*9.81/4; % Load on main wheel in Newton
 D_moleo = 1.3 * sqrt((4*W_moleo)/(P_oleo*pi));
 D_moleo = 0.04*sqrt(W_moleo*0.224809)*0.0254;
+% Nose wheel
+W_noleo = (W_ng_max+W_ng_braking)*9.81; % Load on nose wheel in Newton
+D_noleo = 0.04*sqrt(W_noleo*0.224809)*0.0254;
 
 
 %ACN - Aircraft Classification Number

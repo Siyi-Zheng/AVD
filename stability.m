dc8909eaa6908c21e342386545f15ad74ecca2b0
ben-groat <ben.groat22@imperial.ac.uk>
2024-11-21 22:00:43 +0000

    updated plotting code

--- stability.m
+++ stability.m
@@ -6,7 +6,6 @@ close all
 mac = 7.4; % aircraft mean aero chord
 xcg = 39.40; % aircraft cg
 claw = 5.581; % wing lift curve slope
-hlaw = 4.39; % tailplane lift curve slope
 xacw = 39.6; % aero centre of wing
 kf = 1.4; % some bullshit constant
 lf = 77.82; % fuselage length
@@ -16,7 +15,7 @@ etah = 0.9; % tailplane efficiency
 clah = 4.39; % tailplane lift curve slope
 AR = 8.77; % wing aspect ratio
 lam = 0.25; % wing taper ratio
-w_twist = 3; %in degrees
+w_twist = 3;
 b = 65; % wingspan
 hh = 1.55; % vertical position of hstab
 xach = 74.1; % aero centre of hstab
@@ -31,7 +30,7 @@ compressibility_factor_landing = 1.024;
 compressibility_factor_takeoff = 1.017;
 density_cruise = 0.38;
 cruise_velocity = 254;
-S_w = 440;
+S_w = sw; % why the fuck did you do define S_w as 440 and sw as 482 lol
 Cd = 0.0161 + 0.012 + 0.003; %cd at cruise conditions of aircraft
 alpha_0_w = -4.8 * pi / 180; %X-foil
 alpha_0_h = 1.8 * pi / 180; % its upside-down
@@ -60,19 +59,6 @@ dcmcg_dalpha = (-claw .* (xacw - xcg) / mac + cmaf - etah * clah *...
     (1 - deda_cruise) * (sh / sw) * (xach / mac)) / (claw + etah * clah...
     * (1 - deda_cruise) * (sh / sw));
 
-% % trim analysis (clean (a/c))
-% 
-% CMow = (Cmoairf * (AR * cosd(sweep) ^ 2 / (AR + 2 * cosd(sweep))) -0.01 * w_twist) * compressibility_factor_cruise; %steady level flight
-% q = density_cruise * cruise_velocity ^ 2 * 0.5;
-% Thrust = q * S_w * Cd;
-% 
-% alpha = [-5:1:20];
-% ih = [-3:1:15];
-% 
-% Clw = claw * (alpha + iw - alpha_0_w);
-% C_L_h = hlaw * ((alpha + iw - alpha_0_w) * (1 - deda_cruise) + (ih - iw) - alpha_0_h - alpha_0_w) + maybeelavator;
-% 
-% Cmcg = -Clw * (xacw - xcg) / (mac) + CMow + 0 - etah * C_L_h * (sh / S_w) * ((xach -xcg) / mac) + zt * Thrust / (q * S_w * mac);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%takeoff
 deda_takeoff = (4.44 * ((1 / AR - 1 / (1 + AR ^ 1.7)) ...
@@ -102,45 +88,37 @@ CMow = (Cmoairf * (AR * cosd(sweep) ^ 2 / (AR + 2 * cosd(sweep))) -0.01...
 q = density_cruise * cruise_velocity ^ 2 * 0.5;
 Thrust = q * S_w * Cd;
 
-plotting_list_x = []; % [[ih1a1 ih1a2 ih1a3...][ih2a1 ih2a2 ih2a3...]...]
-plotting_list_y = []; % [[ih1a1 ih1a2 ih1a3...][ih2a1 ih2a2 ih2a3...]...]
+figure; % create plot of CL vs CMcg
 
-for alpha = -5:1:20 % i know this isnt best practice but i dont want to convert all variables to arrays
+for ih = -9.5
+    % convert to radians
+    ih = ih * pi / 180;
 
+    temp_list_cl = []; % stores values for plotting
+    temp_list_cmcg = []; % stores values for plotting
+
+    for alpha = 1
+    % i know this isnt best practice but i dont want to convert all variables to arrays
     % convert to radians
     alpha = alpha * pi / 180;
-   
-    temp_list_x = []; % store CL values here for a specific ih
-    temp_list_y = []; % store CMcg values here for a specific ih
-
-    for ih = -10:1:25
-
-        % convert to radians
-        ih = ih * pi / 180;
 
         % get coefficients
         Clw = claw * (alpha + iw - alpha_0_w);
         C_L_h = clah * ((alpha + iw - alpha_0_w) * (1 - deda_cruise)...
             + (ih - iw) - alpha_0_h - alpha_0_w);
         C_L = Clw + etah * sh / sw * C_L_h;
-        Cmcg = -Clw * (xacw - xcg) / (mac) + CMow + 0 - etah * C_L_h...
+        Cmcg = -Clw * ((xacw - xcg) / mac) + CMow + cmaf * alpha - etah * C_L_h...
             * (sh / S_w) * ((xach - xcg) / mac) + hh * Thrust / (q * S_w * mac);
 
-        % plotting
-        temp_list_x = [temp_list_x C_L];
-        temp_list_y = [temp_list_y Cmcg];
+        % plot
+        temp_list_cl = [temp_list_cl, C_L];
+        temp_list_cmcg = [temp_list_cmcg, Cmcg];
     end
-    plotting_list_x = [plotting_list_x; temp_list_x];
-    plotting_list_y = [plotting_list_y; temp_list_y];
+
+    % plot the line
+    plot(temp_list_cl, temp_list_cmcg, '-x', color="black");
+    hold on;
 end
 
-% create a plot of C_L vs C_Mcg
-figure
-hold on
-% iterate over the values of ih
-for i = 1:size(plotting_list_x, 1)
-    % plot the values of C_L vs C_Mcg for a specific ih as alpha changes
-    plot(plotting_list_x(i, :), plotting_list_y(i, :), "-x", color="blue")
-end
-xlabel("C_L")
-ylabel("C_{Mcg}")
+% trimmed state (cruise) = ih = -9.5 (!!!), alpha = 1. In this case, CLh =
+% -0.2405, CLw = 0.5650, CL = 0.5344

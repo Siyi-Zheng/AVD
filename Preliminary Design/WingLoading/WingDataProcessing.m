5168bcc87946f4f124a313844ae8fbb8fecd6b91
lucjames <lej21@ic.ac.uk>
2025-01-29 13:19:14 +0000

    Updates

--- Preliminary Design/WingLoading/WingDataProcessing.m
+++ Preliminary Design/WingLoading/WingDataProcessing.m
@@ -7,6 +7,8 @@ clc
 heightsm = 0:100:40000;
 H = convlength(heightsm,'ft','m');
 
+Sref = 488.545;
+
 format long g;
 
 [T,a,P,rhos,nu] = atmosisa(H);
@@ -26,8 +28,8 @@ Red = (12.41 .* rhos .* Vd) ./ mu;
 
 L = 354000 * 2.5 * 9.81;
 
-Cl_requireda = L ./ (0.5 * 488.545 * Va^2 .* rhos);
-Cl_requiredd = L ./ (0.5 * 488.545 * Vd^2 .* rhos);
+Cl_requireda = L ./ (0.5 * Sref * Va^2 .* rhos);
+Cl_requiredd = L ./ (0.5 * Sref * Vd^2 .* rhos);
 
 
 
@@ -41,11 +43,11 @@ v = 130.7;
 rho = 1.225;
 
 %load AOA
-alpha = readmatrix("LoadCase1.csv" , 'Range', 'A8:A62');
+alpha = readmatrix("LoadCase1.csv" , 'Range', 'A12:A68')';
 
 %load wing data from CSV file
-Spans = readmatrix("MainWing_a=-4.00_v=130.70ms.csv" , 'Range' , 'A22:A265');
-Chords = readmatrix("MainWing_a=-4.00_v=130.70ms.csv" , 'Range' , 'B22:B265');
+Spans = readmatrix("MainWing_a=-4.00_v=130.70ms.csv" , 'Range' , 'A22:A265')';
+Chords = readmatrix("MainWing_a=-4.00_v=130.70ms.csv" , 'Range' , 'B22:B265')';
 Cl(1,:) = readmatrix("MainWing_a=-4.00_v=130.70ms.csv" , 'Range' , 'D22:D265');
 Cl(2,:) = readmatrix("MainWing_a=-3.50_v=130.70ms.csv" , 'Range' , 'D22:D265');
 Cl(3,:) = readmatrix("MainWing_a=-3.00_v=130.70ms.csv" , 'Range' , 'D22:D265');
@@ -100,7 +102,10 @@ Cl(51,:) = readmatrix("MainWing_a=21.00_v=130.70ms.csv" , 'Range' , 'D22:D265');
 Cl(52,:) = readmatrix("MainWing_a=21.50_v=130.70ms.csv" , 'Range' , 'D22:D265');
 Cl(53,:) = readmatrix("MainWing_a=22.00_v=130.70ms.csv" , 'Range' , 'D22:D265');
 Cl(54,:) = readmatrix("MainWing_a=22.50_v=130.70ms.csv" , 'Range' , 'D22:D265');
-Cl(55,:) = readmatrix("MainWing_a=22.50_v=130.70ms.csv" , 'Range' , 'D22:D265');
+Cl(55,:) = readmatrix("MainWing_a=23.00_v=130.70ms.csv" , 'Range' , 'D22:D265');
+
+
+
 
 %removing fuselage lift
 I = (-2.55 < Spans) & (Spans < 2.55);
@@ -110,13 +115,15 @@ Cl(: , I) = 0;
 
 %removing lift in central section and considering only one half of the wing
 I = find(Spans>2.55, 1 );
-Cl_half = Cl( : , I : end);
+Cl_half = Cl( : , 123 : end);
+
+% = 123;
 
 %converting to sectional lift
 for i = 1:55
 
-    Sect_L(i,:) = Cl(i,:) * 0.5 * rho * v^2 .* Chords';
-    Sect_Lhalf(i,:) = Cl_half(i,:) * 0.5 * rho * v^2 .* Chords(I:end)';
+    Sect_L(i,:) = Cl(i,:) * 0.5 * rho * v^2 .* Chords;
+    Sect_Lhalf(i,:) = Cl_half(i,:) * 0.5 * rho * v^2 .* Chords(123:end);
 
 end
 
@@ -141,11 +148,11 @@ Lifts = zeros(1, 55); % Where `n` is the number of iterations
 
 for i = 1:size(Sect_Lhalf , 1)
 
-    Lifts(i) = 2 * trapz(Spans(I:end)' , Sect_Lhalf(i , :));
+    Lifts(i) = 2 * trapz(Spans(123:end)' , Sect_Lhalf(i , :));
 
 end
 
-
+Cls = (Lifts ./ (0.5 * rho * v^2 * Sref))';
 
 %best AoA to abtain a loading of 2.5g
 [~ , ind] = min(abs(Lifts - L));
@@ -157,20 +164,20 @@ disp(['The aoa to get the correct load factor for Va is ' , num2str(aoa_opt) , '
 %Converting to correct disctretisation
 xq = [2.55 : 0.01 : 32.5 , 32.5];
 
-vq = interp1(Spans(I:end) , Sect_Lhalf(ind,:) , xq , "pchip");
+vq = interp1(Spans(123:end) , Sect_Lhalf(ind,:) , xq , "pchip");
 
 %vq(1:255) = 0;
 
 figure
 hold on
 
-plot(Spans(I:end) , Sect_Lhalf(ind , :) , 'LineStyle','none' , 'Marker','*' ,  'DisplayName', 'Data')
+plot(Spans(123:end) , Sect_Lhalf(ind , :) , 'LineStyle','none' , 'Marker','*' ,  'DisplayName', 'Data')
 plot(xq , vq , 'LineStyle','--' , 'Marker' , 'none' ,  'DisplayName', 'Fit')
 
-xlim([0 40])
-ylim([-20000 160000])
 
 hold off
+
+vq = vq .* 1.0228;
 
 %check it's correct
 Lift_fit = 2 * trapz(xq , vq);

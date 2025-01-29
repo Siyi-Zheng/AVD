8926e45206929e6cf22c5b79225611bb47b17775
ben-groat <ben.groat22@imperial.ac.uk>
2025-01-29 13:59:59 +0000

    more optimisation

--- Preliminary Design/skin.m
+++ Preliminary Design/skin.m
@@ -27,12 +27,12 @@ c = (0.7 - 0.12) .* 12.05; % m
 b2 = 1.51; % m
 E = 70; % GPa
 N = momentMax / (c .* b2); % N/m compressive load per unit length
-flangeWebRatio = 0.3; % ratio of flange length to web height
+flangeWebRatio = 0.7; % ratio of flange length to web height
 
-n = 20: 1: 50;
+n = 5: 1: 50;
 % ts = linspace(1,15, length(n)); % mm      %stringer thickness
 % h = linspace(20, 300, length(n)); % mm      %stringer web height
-ts= 5:0.1:15;
+ts= 1:0.1:15;
 h = 90:1:110;
 b = c ./ n.* 1000; % mm
 d = h .* flangeWebRatio; % mm  %flange width of stringer
@@ -44,7 +44,7 @@ for i = 1:length(n)
             t2(i,j,k)= (N /3.62/ (E*10^9) * (b(i) / 1000) ^ 2) ^ (1/3) *1000; % mm
             sigma_0(i,j,k) = N / (t2(i,j,k) * 1000); % MPa
             t_e(i,j,k) = t2(i,j,k) + (As(i,j,k) / b(i)); % mm
-            effectivePanelCSA = n(i)* b(i)* t_e(i,j,k); % mm^2
+            effectivePanelCSA(i,j,k) = n(i)* b(i)* t_e(i,j,k); % mm^2
             % catchpole diagram
             catchpoleY(i,j,k) = ts(j)/ t2(i,j,k); % x-value to read
             catchpoleX(i,j,k) = As(i,j,k)/ (b(i)* t2(i,j,k)); % y-value to read
@@ -73,6 +73,9 @@ for i = 1:length(n)
     end
 end
 
+% shitty mass optimisation
+% F_factor = F_factor ./ As;
+
 sigma_cr = stressRatio .* sigma_0;
 
 %Farrar efficiency (stringer local buckling)
@@ -87,12 +90,14 @@ sigma_cr = stressRatio .* sigma_0;
 F_factor(F_factor <= 0.5) = NaN;
 for i = 1:length(h)
     slice = squeeze(F_factor(:,:,i));
+    % mass = squeeze(effectivePanelCSA(:,:,i));
     xData = n;
     yData = ts;
-    surf(xData, yData, slice')
+    surf(n, ts, slice')
+    % surf(n, ts, mass')
     hold on
 end
-xlabel("Panel Width (mm)")
+xlabel("Number of Stringers")
 ylabel("Stringer Thickness (mm)")
 zlabel("Farrar Efficiency")
 
@@ -103,6 +108,8 @@ zlabel("Farrar Efficiency")
 n = n(id1);
 ts = ts(id2);
 h = h(id3);
+b = c ./ n.* 1000; % mm
+As = As(id1, id2, id3);
 
 % spar
 
@@ -130,13 +137,12 @@ R_s = tau_0 / tau_cr; % Shear stress ratio
 val = R_s^2 + R_c; % combined stress ratio
 
 % ribs
-M = 43627170; % bending moment, Nm
-c = 13.99; % chord, m
-t_e = 17.84; % equivalent panel thickness, mm (FROM SKIN)
+M = momentMax;
+t_e = ts + (As / b);
 s = 2; % rib spacing, m
 I = (c * (t_e / 1000) ^ 3 / 12 + c * (t_e / 1000) * (b2 / 2) ^ 2); % 2nd moment of area of panel, m^4
 F = M^2 * s * b2 * (t_e / 1000) * c / (2 * E * 1e9) / (I ^ 2); % Crush load, N
 t_r = 7.3; % design rib thickness, mm
 sigma_c = (F / t_r) * (c / 1000); % crush stress, MPa
 sigma_b = F / (t_r * c * 1e3); % buckling stress, MPa
-t_r2 = F / (sigma_y * c * 1e3); % design rib thickness (again???), MPa
+t_r2 = F / (sigma_y * c * 1e3); % design rib thickness (for buckling), MPa
